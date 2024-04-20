2cc52e352b1ccf459385565d9318f87e7e692459
NicoleCoding <nicole251097@gmail.com>
2024-04-20 22:25:39 +0300

    Header and nav are now added via HTML, not JavaScript

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -4,20 +4,10 @@ import { getContact } from './pages/contact';
 
 // Module which loads the page. 
 export function pageLoad() {
-    // Creates the header and adds relevant content inside the header.
-    const header = document.createElement('header');
-    const headerText = document.createElement('h1');
-    headerText.textContent = "Restaurant Luigi";
-    header.appendChild(headerText);
-    // Creates the navigation.
-    const nav = document.createElement('nav');
    
     // Reference to the content element.
     const contentElement = document.getElementById('content');
     
-    // Appends the header and nav to the content element.
-    contentElement.appendChild(header);
-    contentElement.appendChild(nav);
     
     // Retrieves the content for the home, menu and contact pages.
     const homeContent = getHome();
