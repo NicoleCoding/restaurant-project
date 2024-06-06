6735f110955ce7325bca3ff8fb197529430884ad
NicoleCoding <nicole251097@gmail.com>
2024-06-06 22:56:59 +0300

    Fixed footer, created basic contact form and basic styling for submit button

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,30 @@ export function getContact() {
 
     const contactHeading2 = document.createElement('h2');
     contactHeading2.textContent = "Contact us";
+    const contactForm = document.createElement('form');
+    const contactName = document.createElement('input');
+    contactName.type = 'text';
+    contactName.name = 'name';
+    contactName.placeholder = 'Your name';
+    const contactEmail = document.createElement('input');
+    contactEmail.type = 'email';
+    contactEmail.name = 'email';
+    contactEmail.placeholder = 'Your email';
+    const contactPhone = document.createElement('input');
+    contactPhone.type = 'tel';
+    contactPhone.name = 'phone';
+    contactPhone.placeholder = 'Your phonenumber';
+    const submitButton = document.createElement('submit');
+    submitButton.setAttribute('id', 'submitbutton');
+    submitButton.type = 'submit';
+    submitButton.textContent = "Submit";
+    contactForm.appendChild(contactName);
+    contactForm.appendChild(contactEmail);
+    contactForm.appendChild(contactPhone);
+    contactForm.appendChild(submitButton);
 
     contactContent.appendChild(contactHeading2);
+    contactContent.appendChild(contactForm);
 
 
     return contactContent;
