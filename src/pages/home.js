7cb83d2f5f1e2d79a717320be09d128997c428a1
NicoleCoding <nicole251097@gmail.com>
2024-06-26 19:03:30 +0300

    - Changed colors on background and text - Images are now handled through Webpack -  Created hero section (work in progress) - Adjusted header height

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,27 @@
+import heroImage from '../assets/jakub-kapusnak-tEVisOXz26Y-unsplash.jpg';
 
 // The module for the home page.
 export function getHome() {
     // Elements declarations
     const homeContent = document.createElement('div');
     const homeHeading = document.createElement('h2');
-    const introHomeContainer = document.createElement('div');
-    const homeIntro = document.createElement('p');
+    const heroSection = document.createElement('section');
+    const heroText = document.createElement('p');
 
     // Adding content to the elements
     homeHeading.textContent = "Welcome to Restaurant Luigi!";
-    introHomeContainer.setAttribute('id', 'intro');
-    homeIntro.setAttribute('id', 'introductiontext');
-    homeIntro.textContent = "Step into our restaurant and experience the true essence of Italian cooking. From homemade pasta crafted with the finest flour to wood-fired pizzas topped with the freshest ingredients, each dish is a celebration of Italy's culinary traditions. Our menu showcases a diverse array of antipasti, primi, secondi, and dolci, all prepared with passion and precision.";
+    heroSection.setAttribute('id', 'hero');
+    heroText.setAttribute('id', 'hero-text');
+    heroText.textContent = "Step into our restaurant and experience the true essence of Italian cooking. From homemade pasta crafted with the finest flour to wood-fired pizzas topped with the freshest ingredients, each dish is a celebration of Italy's culinary traditions. Our menu showcases a diverse array of antipasti, primi, secondi, and dolci, all prepared with passion and precision.";
     
+    heroSection.style.backgroundImage = `url(${heroImage})`;
+    heroSection.style.backgroundSize = 'cover';
+    heroSection.style.backgroundPosition = 'center';
+
     // Appending the elements to the home content
-    introHomeContainer.appendChild(homeIntro);
+    heroSection.appendChild(heroText);
     homeContent.appendChild(homeHeading);
-    homeContent.appendChild(introHomeContainer);
+    homeContent.appendChild(heroSection);
 
     return homeContent;
 }
