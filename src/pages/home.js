efe437fbb4006d44c10347675470a50c603d5765
NicoleCoding <nicole251097@gmail.com>
2024-06-27 21:47:21 +0300

    Added action button in the hero section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import heroImage from '../assets/jakub-kapusnak-tEVisOXz26Y-unsplash.jpg';
+import { pageLoad } from '../pageLoad';
 
 // The module for the home page.
 export function getHome() {
@@ -7,13 +8,20 @@ export function getHome() {
     const homeHeading = document.createElement('h2');
     const heroSection = document.createElement('section');
     const heroText = document.createElement('p');
+    const heroButton = document.createElement('button');
 
     // Adding content and attributes to the elements
     homeHeading.textContent = "Welcome!";
     heroSection.setAttribute('id', 'hero');
     heroText.setAttribute('id', 'hero-text');
     heroText.textContent = "Step into our restaurant and experience the true essence of Italian cooking. From homemade pasta crafted with the finest flour to wood-fired pizzas topped with the freshest ingredients, each dish is a celebration of Italy's culinary traditions. Our menu showcases a diverse array of antipasti, primi, secondi, and dolci, all prepared with passion and precision.";
-    
+    heroButton.textContent = "Explore our menu";
+    heroButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        pageLoad('menu');
+        
+    });
+
     // Add background to hero section
     heroSection.style.backgroundImage = `url(${heroImage})`;
     heroSection.style.backgroundSize = 'cover';
@@ -22,6 +30,7 @@ export function getHome() {
     // Appending the elements to the home content
     heroSection.appendChild(homeHeading);
     heroSection.appendChild(heroText);
+    heroSection.appendChild(heroButton);
     homeContent.appendChild(heroSection);
 
     return homeContent;
